1d92ef7968b7353b9485818998de34b39366dc8e
Mike H <matroxmike@icloud.com>
2019-09-17 08:59:57 +1000

    some experiments to fix

---
+++ iter5.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let numbers = vec![1i32];
+    let other_numbers: Vec<i32> = numbers.iter().collect();
+
+
+let the_vocabulary: Vec<&Vocabulary> = vocabulary_context
+    .vocabularies
+    .iter()
+    .filter(|voc| voc.metadata.identifier == vocabulary_id)
+    .collect();
+}
+
--- iterators4.rs
+++ iterators4.rs
@@ -7,6 +7,6 @@ fn main() {
   */  
       (0..10).flat_map(|x| x * 100 .. x * 110)
       .enumerate().filter(|&(i, x)| (i + x) % 3 == 0)
-      .for_each(|(i,x)| println!("{}:{}", i, x));
+      .for_each(|(i, x)| println!("{} xx {}",  i, x));
  //     println!("asd");
 }
