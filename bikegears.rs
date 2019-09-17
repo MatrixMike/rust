059828cd36b68c46bf394e21c2947054eccf103d
Mike H <matroxmike@icloud.com>
2019-09-17 16:23:43 +1000

    from Camel to Snake case

--- bikegears.rs
+++ bikegears.rs
@@ -1,9 +1,10 @@
 use std::env;
 // 17.09.2019 16:07:26
+// camel case should be changed for snake case
 fn main() {
-let frontGearVector = vec!(25,35, 52);	
+let _front_gear_vector = vec!(25,35, 52);	
 let  rearGearVector = vec!(12,14,16,18,22,24,30);		
-let newVector = rearGearVector.iter().map(|&x| x +1).collect::<Vec<i32>>();
+let new_vector = rearGearVector.iter().map(|&x| x +1).collect::<Vec<i32>>();
  println!("{:?}", rearGearVector);
- println!("{:?}", newVector); 
+ println!("{:?}", new_vector); 
 }
