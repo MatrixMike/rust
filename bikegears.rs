c74852995a2a9335c44f1ee07fb30a1396f8e1a0
Mike H <matroxmike@icloud.com>
2019-09-17 16:28:15 +1000

    add _ to snake case

--- bikegears.rs
+++ bikegears.rs
@@ -3,8 +3,8 @@
 // camel case should be changed for snake case
 fn main() {
 let _front_gear_vector = vec!(25,35, 52);	
-let  rearGearVector = vec!(12,14,16,18,22,24,30);		
-let new_vector = rearGearVector.iter().map(|&x| x +1).collect::<Vec<i32>>();
- println!("{:?}", rearGearVector);
+let  _rear_gear_vector = vec!(12,14,16,18,22,24,30);		
+let new_vector = _rear_gear_vector.iter().map(|&x| x +1).collect::<Vec<i32>>();
+ println!("{:?}", _rear_gear_vector);
  println!("{:?}", new_vector); 
 }
