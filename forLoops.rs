865bebf9f606c74d93601a9cd94d167a394660c6
Mike H <matroxmike@icloud.com>
2019-09-18 14:28:21 +1000

    after Rustfmt

--- forLoops.rs
+++ forLoops.rs
@@ -2,7 +2,7 @@
 fn main() {
     let mut arr = [0; 16];
     let slice: &mut [i32] = &mut arr;
-    
+
     let slice_len = slice.len();
     for i in 2..slice_len {
         for j in (i * 2)..slice_len {
