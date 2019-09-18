a85d159f2ad12a8a9e35d19a00e0a6656e760b0d
Mike H <matroxmike@icloud.com>
2019-09-18 18:03:18 +1000

    sorted

--- bikegears.rs
+++ bikegears.rs
@@ -5,12 +5,12 @@ fn main() {
     let _vec_of_float = vec![2.0, 3.0];
     let _front_gear_vector = vec![25.0, 35.0, 52.0];
     let _rear_gear_vector = vec![12.0, 14.0, 16.0, 18.0, 22.0, 24.0, 30.0];
-    let mut unsorted_integers = vec![9, 5, 7, 2, 1, 11];
+    let mut unsorted_integers = vec![9, 5, -7, 2, -1, 11];
     let new_vector = _rear_gear_vector
         .iter()
         .map(|&x| x / 2.0)
         .collect::<Vec<f32>>();
-       unsorted_integers.sort() ;
+    unsorted_integers.sort();
     let sorted_integers = unsorted_integers;
     println!("{:?}", sorted_integers);
     println!("{:?}", _rear_gear_vector);
