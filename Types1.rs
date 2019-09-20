661feb74da9500a589dc9c7d325ec353fe8f2eda
Mike H <matroxmike@icloud.com>
2019-09-20 14:25:05 +1000

    format and iterators

--- Types1.rs
+++ Types1.rs
@@ -6,4 +6,5 @@ struct Point<T> {
 fn main() {
     let integer = Point { x: 5, y: 10 };
     let float = Point { x: 1.0, y: 4.0 };
+ //   println!("{}", integer);
 }
--- iterators4.rs
+++ iterators4.rs
@@ -1,12 +1,22 @@
 #![allow(unused)]
 fn main() {
-/*(0..5).flat_map(|x| x * 100 .. x * 110)
-      .enumerate()
-      .filter(|&(i, x)| (i + x) % 3 == 0)
-      .for_each(|(i, x)| println!("{}:{}", i, x));
-  */  
-      (0..10).flat_map(|x| x * 100 .. x * 110)
-      .enumerate().filter(|&(i, x)| (i + x) % 3 == 0)
-      .for_each(|(i, x)| println!("{} xx {}",  i, x));
- //     println!("asd");
+    /*(0..5).flat_map(|x| x * 100 .. x * 110)
+        .enumerate()
+        .filter(|&(i, x)| (i + x) % 3 == 0)
+        .for_each(|(i, x)| println!("{}:{}", i, x));
+    */
+
+    (0..10)
+        .flat_map(|x| x * 100..x * 110)
+        .enumerate()
+        .filter(|&(i, x)| (i + x) % 3 == 0) // first invocation
+        .filter(|&(i, x)| (i + x) % 5 == 0) // copied above and changed - think fizz,buzz
+        .for_each(|(i, x)| println!("{} xx {}", i, x));
+    //     println!("asd");
+    let lines_vec = vec!["hello,how", "are,you"];
+    let words_vec = lines_vec
+        .iter()
+        .flat_map(|&x| x.split(","))
+        .collect::<Vec<&str>>();
+    println!("{:?}", words_vec);
 }
